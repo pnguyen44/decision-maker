c54f6e3707096a19fb0983554b2cb23852d18094
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-26 03:28:56 -0400

    Add event hander to create list button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,10 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const store = require('./store')
+const authEvents = require('./auth/events')
+const listEvents = require('./lists/events')
+const itemEvents = require('./items/events')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +16,31 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$(() => {
+  store.isSignedIn = false
+  // $('#btn-save-list').on('click', function () {
+  //   console.log('there i am')
+  // })
+  authEvents.addHandlers()
+  listEvents.addHandlers()
+  itemEvents.addHandlers()
+  $('#btn-account').on('click', function () {
+    $('#message-form').text('')
+    console.log('is sign in = ', store.isSignedIn)
+    if (store.isSignedIn === true) {
+      $('#change-password').show()
+      $('#view-history').hide()
+      $('#btn-sign-out').show()
+      $('#sign-up').hide()
+      $('#sign-in').hide()
+    } else {
+      $('#sign-up').show()
+      $('#sign-in').show()
+      $('#btn-sign-out').show()
+      $('#change-password').hide()
+      $('#view-history').hide()
+      $('#btn-sign-out').hide()
+    }
+  })
+})
