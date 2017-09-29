299017934f82d9b29e1b0ee8463fb2e424995d9f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-29 14:11:59 -0400

    Add choose item function - add onChooseItem function

--- assets/scripts/lists/ui.js
+++ assets/scripts/lists/ui.js
@@ -5,7 +5,6 @@ const listsEvents = require('./events')
 const itemsEvents = require('../items/events')
 const listsApi = require('./api')
 const listsUi = require('./ui')
-const app = require('../app')
 const showListsTemplate = require('../templates/list-listing.handlebars')
 const onSuccess = function (data) {
 }
@@ -29,58 +28,72 @@ const getListsSuccess = function (data) {
   })
 
   $('.btn-add-item').on('click', function (e) {
-    e.preventDefault()
-
-    const listId = $(this).attr('data-item-name-list-id')
-    const inputBox = `#item-name-list-id-${listId}`
-    const itemName = String($(inputBox).val())
-    $(inputBox).val('')
-    itemsEvents.createOneItem(itemName, false, listId)
+    if (store.isSignedIn === true) {
+      e.preventDefault()
+      const listId = $(this).attr('data-item-name-list-id')
+      const inputBox = `#item-name-list-id-${listId}`
+      const itemName = String($(inputBox).val())
+      $(inputBox).val('')
+      itemsEvents.createOneItem(itemName, false, listId)
+    }
   })
+
   $('.btn-delete-item').on('click', function () {
-    const itemId = $(this).attr('data-item-delete-id')
-    itemsEvents.onDeleteItem(itemId)
+    if (store.isSignedIn === true) {
+      const itemId = $(this).attr('data-item-delete-id')
+      itemsEvents.onDeleteItem(itemId)
+    }
   })
 
   $('.btn-delete-list').on('click', function () {
-    const listId = $(this).attr('data-list-delete-id')
-    listsEvents.onDeleteList(listId)
+    if (store.isSignedIn === true) {
+      const listId = $(this).attr('data-list-delete-id')
+      listsEvents.onDeleteList(listId)
+    }
   })
 
   $('.checkBox').on('click', function () {
-    const mark = $(this).is(':checked')
-    const itemName = $(this).attr('data-item-name-id')
-    const itemId = $(this).attr('data-checkbox-item-id')
-    const listId = $(this).attr('data-list-id')
-    const value = $(this).val()
-    itemsEvents.onUpdateItem(itemName, mark, itemId, listId)
+    if (store.isSignedIn === true) {
+      const mark = $(this).is(':checked')
+      const itemName = $(this).attr('data-item-name-id')
+      const itemId = $(this).attr('data-checkbox-item-id')
+      const listId = $(this).attr('data-list-id')
+      const value = $(this).val()
+      itemsEvents.onUpdateItem(itemName, mark, itemId, listId)
+    }
   })
   $('.btn-edit-list').on('click', function () {
-    const listId = $(this).attr('data-list-edit-id')
-    store.listId = listId
-    const listName = $(this).attr('data-list-edit-name')
-    $('.edit-list-input').val(listName)
-    $('#edit-list').modal('show')
+    if (store.isSignedIn === true) {
+      const listId = $(this).attr('data-list-edit-id')
+      store.listId = listId
+      const listName = $(this).attr('data-list-edit-name')
+      $('.edit-list-input').val(listName)
+      $('#edit-list').modal('show')
+    }
   })
   $('.btn-edit-item').on('click', function () {
-    store.editItemId = $(this).attr('data-item-edit-id')
-    store.editItemName = $(this).attr('data-item-edit-name')
-    store.editItemlistId = $(this).attr('data-item-edit-list-id')
-    store.editItemMark = $(this).attr('data-item-edit-mark')
+    if (store.isSignedIn === true) {
+      store.editItemId = $(this).attr('data-item-edit-id')
+      store.editItemName = $(this).attr('data-item-edit-name')
+      store.editItemlistId = $(this).attr('data-item-edit-list-id')
+      store.editItemMark = $(this).attr('data-item-edit-mark')
 
-    $('.edit-item-input').val(store.editItemName)
-    $('#edit-item').modal('show')
+      $('.edit-item-input').val(store.editItemName)
+      $('#edit-item').modal('show')
+    }
   })
   $('.btn-choose-item').on('click', function () {
-    store.chooseItemListId = $(this).attr('data-choose-item-list-id')
-    listsEvents.getOneList(store.chooseItemListId)
+    if (store.isSignedIn === true) {
+      store.chooseItemListId = $(this).attr('data-choose-item-list-id')
+      listsEvents.getOneList(store.chooseItemListId)
+    }
   })
 }
 
 const getOneListSuccess = function (data) {
   store.list = data.list
   store.items = data.list.items
-  app.chooseItem(store.list.items)
+  itemsEvents.onChooseItem(store.list.items)
 }
 
 const onUpdateSuccess = function () {
